3bc16f056ce5f41d89cfaa34d04960fbecc79a60
Pipiing <1292379046@qq.com>
2023-06-18 23:19:26 +0800

    添加Redis防重提交Key

--- haolucai-common/common-util/src/main/java/com/chen/common/constant/CacheConstants.java
+++ haolucai-common/common-util/src/main/java/com/chen/common/constant/CacheConstants.java
@@ -12,4 +12,9 @@ public interface CacheConstants
      * 登录账户密码错误次数 redis key
      */
     String PWD_ERR_CNT_KEY = "pwd_err_cnt:";
+
+    /**
+     * 防重提交 redis key
+     */
+    String REPEAT_SUBMIT_KEY = "repeat_submit:";
 }
